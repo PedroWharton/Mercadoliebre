9d8f5225c4e0f15d572d8f0863acba64f6f759e3
PedroWharton <pedrowharton@gmail.com>
2022-06-06 21:55:05 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.use(express.static("public"));
+
+app.listen(3000, function(){
+    console.log("Servidor corriendo");
+})
+
+function get(get, direc){
+    app.get(get, function(req, res){
+        res.sendFile(path.resolve(__dirname, direc))
+    })
+    
+}
+get("/","views/home.html")
+get("/register","views/register.html")
+get("/login","views/login.html")
